3e83d2d88e55b5df0eb476f808f19234935d0b0b
yilianz <yilianz@gmail.com>
2021-10-31 19:10:05 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# workspace-javascript-buygroceries-4hqc3r
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/workspace-javascript-buygroceries-4hqc3r)
---
+++ js/add-element.js
@@ -0,0 +1,31 @@
+/*
+ * Practice on Elements
+ */
+
+// get all the hot class elements- change them to cool class
+
+// traverse the elements
+
+// add a new element by clicking the plus sign
+
+// before and after are for siblings
+// append and prepend are for parent
+
+function addElement() {
+  // add a new element
+  // add a input text box
+  // whenever the user are done add the element
+}
+
+// bind click with the event handler
+
+//  click the li element will change the changeStyle
+//  three style : complete, cool, hot
+function changeStyle() {}
+
+// delete complete element by clicking the trash can
+document.getElementById('remove').addEventListener('click', removeElement);
+
+function removeElement() {
+  // remove the marked element
+}
