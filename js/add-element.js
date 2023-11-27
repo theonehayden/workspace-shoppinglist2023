caab7e4ca71b832c71f1cac0336835a0a1086437
[Hayden] <[theonehayden@gmail.com]>
2023-11-27 12:00:10 -0500

    update

--- js/add-element.js
+++ js/add-element.js
@@ -3,16 +3,20 @@
  */
 
 // get all the hot class elements- change them to cool class
-
+$(".hot").each(function(){
+  //this.setAttribute("class", "cool");
+  $(this).removeClass("hot");
+});
 // traverse the elements
-
+$("#two").next().next().text("Candy");
 // add a new element by clicking the plus sign
-
+$("add").click(addElement);
 // before and after are for siblings
 // append and prepend are for parent
 
 function addElement() {
   // add a new element
+  
   // add a input text box
   
   // whenever the user are done add the element
